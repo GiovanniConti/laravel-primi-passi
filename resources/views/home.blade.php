a9bbb4d0559d37fa0e7d013283cc43227e00ceb9
GiovanniConti <giovanni.conti@live.it>
2021-12-20 10:36:53 +0100

    Linked page aboutUs to Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Homepage</title>
 </head>
 <body>
   <h1> Hello World! </h1>
+
+  <nav>
+    <a href="{{ route('aboutUs') }}">Chi Siamo</a>
+    <a href="">Contatti</a>
+    <a href=""></a>
+    <a href=""></a>
+  </nav>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/aboutUs', function () {
+    return view('aboutUs');
+})->name("aboutUs");
